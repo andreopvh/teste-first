27212325327b87aeb75a8f48deaeee4459b32e6e
p0656622 <andre.luizoliveira@portoseguro.com.br>
2023-09-29 09:45:08 -0300

    criação de controller repository e service da api usuario

---
+++ Usuario/src/main/java/example/Usuario.java
@@ -0,0 +1,27 @@
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Nome é obrigatório")
+    @Size(min = 3, max = 50, message = "Nome deve ter entre 3 e 50 caracteres")
+    private String nome;
+
+    @Email(message = "E-mail inválido")
+    @NotBlank(message = "E-mail é obrigatório")
+    private String email;
+
+    @NotBlank(message = "Senha é obrigatória")
+    @Size(min = 6, max = 20, message = "Senha deve ter entre 6 e 20 caracteres")
+    private String senha;
+
+}
